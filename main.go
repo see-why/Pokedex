12d978b4bc49c3ddfcbf88c280fa99b4d2059661
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-08-04 21:16:46 -0400

    Implement main loop and command handling for Pokedex

--- main.go
+++ main.go
@@ -1,12 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		firstWord := words[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	commands := getCommands()
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+
+	return nil
 }
 
 func cleanInput(text string) []string {
